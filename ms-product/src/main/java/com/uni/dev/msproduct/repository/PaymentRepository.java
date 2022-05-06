23f972c7bb7a63f1645f922456fe5075308b3dea
Juan Gabriel Condori Jara <juan.condori.jara@vallegrande.edu.pe>
2022-05-06 17:33:40 -0500

    Delete ms-product directory

--- ms-product/src/main/java/com/uni/dev/msproduct/repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.uni.dev.msproduct.repository;
-
-import com.uni.dev.msproduct.model.Payment;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PaymentRepository extends MongoRepository<Payment, String> {
-}
--- ms-product/src/main/java/com/uni/dev/msproduct/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.uni.dev.msproduct.repository;
-
-import com.uni.dev.msproduct.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends MongoRepository<Product, String> {
-}

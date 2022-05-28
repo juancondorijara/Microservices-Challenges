0d9e82ffc7c70e84101fa0025df6e11c8b816d41
Juan Gabriel Condori Jara <juan.condori.jara@vallegrande.edu.pe>
2022-05-28 00:15:33 -0500

    Eureka Server

---
+++ EurekaServer/src/test/java/com/vallegrande/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vallegrande.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

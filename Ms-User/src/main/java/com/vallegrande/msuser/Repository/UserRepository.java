75c4bbc86bbe41b2d84c95371d5401444c1830d3
Juan Gabriel Condori Jara <juan.condori.jara@vallegrande.edu.pe>
2022-05-28 00:21:23 -0500

    Delete Ms-User directory

--- Ms-User/src/main/java/com/vallegrande/msuser/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vallegrande.msuser.Repository;
-
-import com.vallegrande.msuser.Domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
